71ed3f075d0cc641a0f49ead0c1737ab92767793
Arthur Burgin Jr <artyymcflyy@users.noreply.github.com>
2022-02-14 22:41:04 -0700

    setup next-api route that handles searching for tvshows to hide external services from UI

---
+++ pages/api/tvshows.ts
@@ -0,0 +1,30 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type TMDBShowResponse = {
+    results: Array<TMDBShowResults>
+}
+
+type TMDBShowResults = {
+    name: string
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<TMDBShowResponse>) {
+    const { query } = req.query
+    const showsResponse: TMDBShowResponse = await searchTVShows(query)
+    return res.status(200).send(showsResponse)
+}
+
+async function searchTVShows(title: string | string[]) {
+    const apiKey = process.env.apiKey
+    const page = 1
+    const language = 'en-US'
+    const includeAdult = false
+    const tmdbUrl = `https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&languge=${language}&page=${page}&query=${title}&include_adult=${includeAdult}`
+    const res = await fetch(tmdbUrl)
+    return res.json()
+}
+
+// function searchMovies() {
+
+// }
