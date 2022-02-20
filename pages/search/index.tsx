9bb086a7a1cd0b193c501feb594ed46e3754ebe6
Arthur Burgin Jr <artyymcflyy@users.noreply.github.com>
2022-02-20 01:59:48 -0700

    rename pages/tvShow to pages/search. This allowed me to keep the page dynamic add more search elements like movies

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,38 +1,56 @@
 import { NextPage } from "next";
 import Link from "next/link";
-import { useState } from "react";
+import { ChangeEventHandler, KeyboardEventHandler, useState } from "react";
 
 
 const Search: NextPage = () => {
-    const [queryParam, setQueryParam] = useState()
+    const [queryParam, setQueryParam] = useState('')
     const [results, setResults] = useState<any[]>()
 
     async function fetchData() {
-        const fetchResponse = await fetch(`/api/tvshow?query=${queryParam}`)
-        const tvShowsResponse = await fetchResponse.json()
-        const tvShowsArr = tvShowsResponse.results
-        const results: any[] = tvShowsArr.map((result: any) => {
-            const title: string = result.name
+        try {
+            let test = []
+            const searchRequests: Promise<Response>[] = [
+                fetch(`/api/search/tv?query=${queryParam}`),
+                fetch(`/api/search/movie?query=${queryParam}`)
+            ]
+            const responses = await Promise.all(searchRequests)
+            // TODO: Find a way to use a loop to fulfill the promises dynamically and save them to a single array
+            const tvShows = await responses[0].json()
+            const movies = await responses[1].json()
+            const allResults = [...tvShows.results, ...movies.results]
 
-            return (
-                <li key={result.id}>
-                    <Link href={`/tvShow?title=${title}`}>{title}</Link>
-                </li>
-            )
-        })
-        setResults(results)
+            const allLinks: any[] = allResults.map((result: any) => {
+                const resultType = result.first_air_date ? "tv" : "movie"
+                const title: string = resultType === "tv" ? result.name : result.title
+                const keyId: string = result.id.toString()
+    
+                return (
+                    <li key={keyId}>
+                        <Link href={`/${resultType}?title=${title}`}>{title}</Link>
+                    </li>
+                )
+            })
+
+            setResults(allLinks)
+        } catch(error) {
+            console.error(error)
+        }
     }
 
-    const onChangeHandler = (event: any) => {
+    const onQueryTitle: ChangeEventHandler = (event: any) => {
         setQueryParam(event.target.value)
+    }
+
+    const OnPressEnter: KeyboardEventHandler = (event) => {
+        if (event.key === 'Enter') fetchData()
     }
 
     return (
         <div>
             <h1>Hello Search!</h1>
             <div>
-                <input type={'text'} onChange={onChangeHandler}/ >
-                <button onClick={fetchData}>Submit</button>
+                <input type={'text'} onChange={onQueryTitle} onKeyPress={OnPressEnter}/ >
             </div>
             <h3>{results}</h3>
         </div>
